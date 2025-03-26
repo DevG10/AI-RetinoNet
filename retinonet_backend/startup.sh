5dc1a4ae06e5115b8f3e547cab4ba149bc671154
Dev Govindani <devgovindani8343@gmail.com>
2025-03-26 13:37:51 +0530

    Fix startup script to install system dependency without the -y flag

--- retinonet_backend/startup.sh
+++ retinonet_backend/startup.sh
@@ -2,7 +2,7 @@
 set -e  # Exit on error
 
 # Install required system dependencies
-apt-get update && apt-get install -y libgl1-mesa-glx
+apt-get update && apt-get install libgl1
 
 # Start Uvicorn server
 uvicorn main:app --host 0.0.0.0 --port $PORT
