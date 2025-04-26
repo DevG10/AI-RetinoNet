bfdb17fe89f17a5f0b29de75fa1d9880b6b0b3ab
Dev Govindani <devgovindani8343@gmail.com>
2025-04-26 21:38:32 +0530

    Refactor Dockerfile to copy start script before application files and remove obsolete startup.sh

--- retinonet_backend/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # Exit on error
-
-# Install required system dependencies
-apt-get update && apt-get install libgl1
-
-# Start Uvicorn server
-uvicorn main:app --host 0.0.0.0 --port $PORT
